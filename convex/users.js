2426370eeab032022632473076e8ca4101d1464c
amandameneseso <amandamenezes594@gmail.com>
2025-11-12 11:21:41 -0300

    adiciona a função createUser

--- convex/users.js
+++ convex/users.js
@@ -1,5 +1,32 @@
 import { v } from "convex/values";
-import { query } from "./_generated/server";
+import { mutation, query } from "./_generated/server";
+
+export const createUser = mutation({
+  args: {
+    clerkId: v.string(),
+    email: v.string(),
+    name: v.string(),
+    profileImage: v.optional(v.string()),
+  },
+  handler: async (ctx, args) => {
+    const exisitingUser = await ctx.db
+      .query("users")
+      .withIndex("by_clerkId", (q) => q.eq("clerkId", args.clerkId))
+      .first();
+
+    if (exisitingUser) {
+      return exisitingUser._id;
+    }
+
+    const userId = await ctx.db.insert("users", {
+      ...args,
+      createdAt: Date.now(),
+      updatedAt: Date.now(),
+    });
+
+    return userId;
+  },
+});
 
 export const getUserByClerkId = query({
   args: { clerkId: v.string() },
