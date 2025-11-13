e20c5bb4fec748d87637c7b7ce40e67928ce06f2
amandameneseso <amandamenezes594@gmail.com>
2025-11-13 20:10:02 -0300

    uploadthing core.js e proxy

--- app/api/uploadthing/core.js
+++ app/api/uploadthing/core.js
@@ -1,16 +1,24 @@
-import { metadata } from "@/app/layout";
+import { auth } from "@clerk/nextjs/server";
 import { createUploadthing } from "uploadthing/next";
 
 const f = createUploadthing();
 
 export const ourFileRouter = {
   petImages: f({
-    image: { maxFileSize: "10MB", maxFileCount: 7 },
-  }).onUploadComplete(async ({ metadata, file }) => {
-    console.log("Upload complete");
-    console.log("File URL:", file.ufsUrl);
-    // console.log("Uploaded by user:", metadata.userId);
+    image: { maxFileSize: "16MB", maxFileCount: 20 },
+  })
+    .middleware(async () => {
+      const { userId } = await auth();
+      if (!userId) {
+        throw new Error("Unauthorized: No Clerk user found");
+      }
+      return { userId };
+    })
+    .onUploadComplete(async ({ metadata, file }) => {
+      console.log("Upload complete");
+      console.log("File URL:", file.ufsUrl);
+      console.log("Uploaded by user:", metadata.userId);
 
-    return { uploadedBy: metadata.userId };
-  }),
+      return { uploadedBy: metadata.userId, fileUrl: file.ufsUrl };
+    }),
 };
